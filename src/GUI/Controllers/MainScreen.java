a8ea6eaa33d6024debc3f10f8d180659d079e81d
alekss <anoviconoks@gmail.com>
2024-02-06 22:23:54 +0100

    When the FXML is open generates new random number

--- src/GUI/Controllers/MainScreen.java
+++ src/GUI/Controllers/MainScreen.java
@@ -8,17 +8,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
 
-public class MainScreen {
+import java.io.IOException;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainScreen implements Initializable {
     @FXML
     private TextField numberfield;
     @FXML
     private Label questlabel;
     @FXML
     private Label levellabel;
+    private int rnumber;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        createRandom();
+    }
+
 
     public void guessnumber(ActionEvent actionEvent) throws IOException {
 
     }
+
+    public void createRandom(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(1000);
+        System.out.println("Random number is"+ randomNumber);
+    }
+
 }
